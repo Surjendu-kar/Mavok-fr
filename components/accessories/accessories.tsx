adbd575ee6da10e0b70b2f6f6c2e91daf17c7c6a
Surjendu kar <rahulgotp9988@gmai.com>
2025-01-09 19:23:16 +0530

    Enhance Accessories component layout and styling
    - Updated the Accessories component to improve layout and responsiveness, including adjustments to padding and font sizes for better readability across devices.
    - Modified image dimensions and classes for accessory items to ensure consistent sizing and alignment.
    - Improved the sorting dropdown options for clarity and consistency in value attributes.
    - Enhanced the overall styling of accessory headings, subheadings, and pricing for a more polished appearance.

--- components/accessories/accessories.tsx
+++ components/accessories/accessories.tsx
@@ -34,36 +34,37 @@ export default function Accessories({ accessoriesType, accessories }: Props) {
               width={50}
               height={50}
               unoptimized
+              className="w-[40px] h-[40px] lg:w-[60px] lg:h-[60px] object-cover mx-auto"
             />
             <p className="font-bold">{type.type}</p>
           </div>
         ))}
       </div>
 
-      <div className="flex flex-col bg-[#FAFAFA] px-10 py-10 gap-6">
+      <div className="flex flex-col bg-[#FAFAFA] px-5 lg:px-10 py-10 gap-6">
         {/* header */}
         <div className="flex justify-between">
-          <p className="text-primary text-2xl font-bold">Accessories</p>
+          <p className="text-primary lg:text-2xl text-xl font-bold">Accessories</p>
           <div className="flex gap-2 items-center">
-            <p className="text-primary-light text-sm">Sort by:</p>
+            <p className="text-primary-light text-xs lg:text-sm font-bold">Sort by :</p>
             <select
               name="sort"
               id="sort"
-              className="text-primary bg-transparent font-bold text-sm lg:text-[13px] cursor-pointer uppercase"
+              className="text-primary bg-transparent font-bold text-xs lg:text-sm cursor-pointer uppercase"
             >
-              <option value="en">Recommendation</option>
-              <option value="ar">Price: Low to High</option>
-              <option value="ar">Price: High to Low</option>
+              <option value="recommendation">Recommendation</option>
+              <option value="price_low_to_high">Price: Low to High</option>
+              <option value="price_high_to_low">Price: High to Low</option>
             </select>
           </div>
         </div>
 
         {/* accessories */}
-        <div className="flex flex-wrap gap-4  items-center justify-center">
+        <div className="flex flex-wrap gap-4 items-center justify-center">
           {filteredAccessories.map(accessory => (
             <div
               key={accessory.id}
-              className="flex flex-col w-[220px] p-6 gap-2 bg-background rounded-lg"
+              className="flex flex-col w-[150px] lg:w-[220px] p-3 lg:p-6 gap-2 bg-background rounded-lg"
             >
               <Image
                 src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${accessory.image}`}
@@ -71,12 +72,12 @@ export default function Accessories({ accessoriesType, accessories }: Props) {
                 width={100}
                 height={100}
                 unoptimized
-                className="w-[100px] h-[100px] object-cover mx-auto"
+                className="w-[60px] h-[60px] lg:w-[100px] lg:h-[100px] object-cover mx-auto"
               />
-              <p className="font-bold uppercase">{accessory.heading}</p>
-              <p className="text-primary-light text-sm">{accessory.sub_heading}</p>
-              <p className="font-bold text-sm uppercase">From ${accessory.price}.00</p>
-              <button className="bg-[#107C11] text-white px-4 py-2 rounded hover:bg-green-600 transition-colors font-bold">
+              <p className="font-bold uppercase text-sm lg:text-base">{accessory.heading}</p>
+              <p className="text-primary-light text-xs lg:text-sm">{accessory.sub_heading}</p>
+              <p className="font-bold text-xs lg:text-sm uppercase">From ${accessory.price}.00</p>
+              <button className="bg-[#107C11] text-sm lg:text-base text-white lg:px-4 lg:py-2 px-0 py-1 rounded hover:bg-green-600 transition-colors font-bold">
                 Add to Cart
               </button>
             </div>
